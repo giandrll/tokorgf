a3765db3329a28727c0a657f8fba47b0cd82254b
PKLGrage <pklgrage@gmail.com>
2024-12-24 14:01:32 +0700

    09

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,6 +10,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Changa:wght@200..800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@600&display=swap" rel="stylesheet">
+
     <link rel="stylesheet" href="customer/css/dashboard.css">
 
 </head>
